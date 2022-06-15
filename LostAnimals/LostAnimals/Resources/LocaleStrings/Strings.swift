ff8839b28c1d1fc3895b34f37fa28213f262e5e3
Miguel Ferrer Fornali <mferrerf@ext.vectoritcgroup.com>
2022-06-15 22:09:48 +0200

    NEW: Add localizables files

---
+++ LostAnimals/LostAnimals/Resources/LocaleStrings/Strings.swift
@@ -0,0 +1,42 @@
+//
+//  Strings.swift
+//  LostAnimals
+//
+//  Created by Miguel Ferrer Fornali on 15/6/22.
+//  Copyright © 2022 Rudo. All rights reserved.
+//
+
+import UIKit
+
+protocol Localizable: CustomStringConvertible {
+    var rawValue: String { get }
+}
+
+extension Localizable {
+    var localized: String {
+        return NSLocalizedString(self.rawValue, comment: "")
+    }
+
+    var uppercased: String {
+        return self.localized.uppercased()
+    }
+
+    var description: String {
+        return self.localized
+    }
+
+    func localized(with: CVarArg...) -> String {
+        let text = String(format: self.localized, arguments: with)
+        return text
+    }
+
+    func callAsFunction() -> String {
+        return self.localized
+    }
+}
+
+extension String {
+    enum Common: String, Localizable {
+        case Test = "Common_Test"
+    }
+}
